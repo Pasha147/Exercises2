e84528c59a364a1bfa769f9f839e390d75e4b865
Pasha <75729255+Pasha147@users.noreply.github.com>
2022-05-24 20:58:22 +0300

    45 minutes

--- CV4/cv4a/src/Components/Intro/Intro.jsx
+++ CV4/cv4a/src/Components/Intro/Intro.jsx
@@ -9,6 +9,7 @@ import boy from "../../img/boy.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
 import glassesimoji from "../../img/glassesimoji.png";
+import { Floatingdiv } from "../Floatingdiv/Floatingdiv";
 
 export const Intro = () => {
   return (
@@ -33,6 +34,13 @@ export const Intro = () => {
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
         <img src={boy} alt="" />
+        {/* 40minutes */}
+        <div style={{ top: "-4%", left: "68%" }}>
+          <Floatingdiv image={crown} text1="Web" text2="Developer" />
+        </div>
+        <div style={{ top: "18rem", left: "0rem" }}>
+          <Floatingdiv image={thumbup} text1="Best Design" text2="Award" />
+        </div>
       </div>
     </div>
   );
