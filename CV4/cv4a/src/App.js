6fcc156376290f406a8d324db67d216c2c8bba43
Pasha <75729255+Pasha147@users.noreply.github.com>
2022-05-24 21:52:04 +0300

    52 minutes

--- CV4/cv4a/src/App.js
+++ CV4/cv4a/src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { Intro } from "./Components/Intro/Intro";
+import { Services } from "./Components/Services/Services";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Services />
     </div>
   );
 }
---
+++ CV4/cv4a/src/Components/Services/Services.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Services.scss";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+
+export const Services = () => {
+  return (
+    <div className="services">
+      <div className="awesome">
+        <span>My Awesome</span>
+        <span>services</span>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
+          dignissimos officiis sint quae mollitia natus rem impedit libero ipsa
+          temporibus.
+        </span>
+      </div>
+      <div className="cards"></div>
+    </div>
+  );
+};
