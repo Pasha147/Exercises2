e53badf1e0230e5eaec46a6ffa103dea5fa8937e
Pasha <75729255+Pasha147@users.noreply.github.com>
2022-06-02 11:01:29 +0300

    Second circle app

---
+++ Circle Text/circle_text/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text" />
+      <div>
+        <div className="textLine">dd</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
