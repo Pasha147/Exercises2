3e5daa8cf01a78f0e6ed1f905aa376258de8e4d8
Pasha <75729255+Pasha147@users.noreply.github.com>
2022-09-01 21:14:29 +0300

    CircleText The universal handle input function

--- Circle Text/circle_text1/src/App.js
+++ Circle Text/circle_text1/src/App.js
@@ -14,51 +14,41 @@ function App() {
 
   const controls = [
     {
-      className: "inp",
-      type: "range",
       name: "radius",
       min: "2",
       max: "400",
-      value: state.radius,
       step: "1",
-      curValue: state.radius,
-      labelEnd: `px`,
+      lable: ["Radius", `px`],
     },
     {
-      className: "inp",
-      type: "range",
       name: "fontSize",
       min: "2",
       max: "100",
-      value: state.fontSize,
       step: "1",
-      curValue: state.fontSize,
-      labelEnd: `px`,
+      lable: ["Font size", `px`],
+    },
+    {
+      name: "angle",
+      min: "0",
+      max: "360",
+      step: "1",
+      lable: ["Angle", `deg`],
+    },
+    {
+      name: "compration",
+      min: "0.1",
+      max: "3",
+      step: "0.1",
+      lable: ["Compration", ` `],
     },
   ];
 
-  const handleInput = (e) => {
-    let text = e.target.value;
-    setState({ ...state, textInput: text });
-  };
-
-  const proportionately = () => {
-    setState({ ...state, propor: !state.propor });
-  };
-  const radHandleChange = (e) => {
-    setState({ ...state, radius: Number(e.target.value) });
-  };
-  const fontHandleChange = (e) => {
-    setState({ ...state, fontSize: Number(e.target.value) });
-  };
-  const angleHandleChange = (e) => {
-    setState({ ...state, angle: Number(e.target.value) });
-  };
-  const comprationHandleChange = (e) => {
-    setState({ ...state, compration: Number(e.target.value) });
-  };
   const handleChangeControls = (e) => {
-    console.log(e.target.name);
+    if (e.target.name === "propor") {
+      setState({ ...state, propor: !state.propor });
+    } else {
+      setState({ ...state, [e.target.name]: e.target.value });
+    }
   };
 
   return (
@@ -70,90 +60,39 @@ function App() {
         <div className="LeftSide">
           <p>Controls</p>
           <div className="Control">
-            <label htmlFor="text">{`Text`}</label>
+            <label htmlFor="textInput">{`Text`}</label>
             <input
-              name="text"
+              name="textInput"
               type="text"
               value={state.textInput}
-              onChange={handleInput}
+              onChange={handleChangeControls}
             />
           </div>
           <div className="Control controlCheck">
             <input
               type="checkbox"
-              name="proportionately"
-              onChange={proportionately}
+              name="propor"
+              onChange={handleChangeControls}
               defaultChecked={state.propor}
             />
             <label htmlFor="roportionately">{`Proportionately`}</label>
           </div>
-          <div className="Control">
-            <label htmlFor="radius">{`Radius ${state.radius}px`}</label>
-            <input
-              className="inp"
-              type="range"
-              name="radius"
-              min="2"
-              max="400"
-              value={state.radius}
-              step="1"
-              onChange={radHandleChange}
-            />
-          </div>
-          <div className="Control">
-            <label htmlFor="fontSize">{`Font size ${state.fontSize}px`}</label>
-            <input
-              className="inp"
-              type="range"
-              name="fontSize"
-              min="2"
-              max="100"
-              step="1"
-              value={state.fontSize}
-              onChange={fontHandleChange}
-            />
-          </div>
-          <div className="Control">
-            <label htmlFor="angle">{`Angle ${state.angle}deg`}</label>
-            <input
-              className="inp"
-              type="range"
-              name="angle"
-              min="0"
-              max="360"
-              value={state.angle}
-              step="1"
-              onChange={angleHandleChange}
-            />
-          </div>
-          <div className="Control">
-            <label htmlFor="compration">{`Compration ${state.compration}`}</label>
-            <input
-              className="inp"
-              type="range"
-              name="compration"
-              min="0.1"
-              max="3"
-              value={state.compration}
-              step="0.1"
-              onChange={comprationHandleChange}
-            />
-          </div>
+
           {controls.map((item, i) => {
             return (
               <div key={item.name} className="Control">
-                <label
-                  htmlFor={item.name}
-                >{`${item.name} ${item.curValue} ${item.labelEnd}`}</label>
+                <label htmlFor={item.name}>{`${item.lable[0]} ${
+                  state[item.name]
+                } ${item.lable[1]}`}</label>
                 <input
-                  className={item.className}
-                  type={item.type}
+                  className="inp"
+                  type="range"
                   name={item.name}
                   min={item.min}
                   max={item.max}
-                  value={item.value}
+                  value={state[item.name]}
                   step={item.step}
-                  onChange={(e) => handleChangeControls(e)}
+                  onChange={handleChangeControls}
                 />
               </div>
             );
--- Circle Text/circle_text1/src/components/CircleComp.js
+++ Circle Text/circle_text1/src/components/CircleComp.js
@@ -12,32 +12,30 @@ export default function CircleComp(props) {
   const [state, setState] = useState({ ...ref.current });
 
   useEffect(() => {
-    if (props.text.textInput.length > 0) {
-      //change abc library and determining width and height
-      const chars = props.text.textInput.split("");
-      const newabc = new Set(chars);
-      newabc.forEach((char) => {
-        if (!ref.current.abc.find((el) => el.char === char)) {
-          charBlockRef.current.innerText = char === " " ? `\u00A0 ` : char;
-          const width = charBlockRef.current.getBoundingClientRect().width;
-          const height = charBlockRef.current.getBoundingClientRect().height;
-          charBlockRef.current.innerText = "";
-          ref.current.abc.push({ char: char, width: width, height: height });
-        }
-      });
-      //========================================
+    //change abc library and determining width and height
+    const chars = props.text.textInput.split("");
+    const newabc = new Set(chars);
+    newabc.forEach((char) => {
+      if (!ref.current.abc.find((el) => el.char === char)) {
+        charBlockRef.current.innerText = char === " " ? `\u00A0 ` : char;
+        const width = charBlockRef.current.getBoundingClientRect().width;
+        const height = charBlockRef.current.getBoundingClientRect().height;
+        charBlockRef.current.innerText = "";
+        ref.current.abc.push({ char: char, width: width, height: height });
+      }
+    });
+    //========================================
 
-      ref.current.text = props.text.textInput;
-      let width = 0;
-      chars.forEach((char) => {
-        width = width + ref.current.abc.find((el) => el.char === char).width;
-      });
+    ref.current.text = props.text.textInput;
+    let width = 0;
+    chars.forEach((char) => {
+      width = width + ref.current.abc.find((el) => el.char === char).width;
+    });
 
-      ref.current.width = width;
-      ref.current.length = props.text.textInput.length;
+    ref.current.width = width;
+    ref.current.length = props.text.textInput.length;
 
-      setState({ ...ref.current });
-    }
+    setState({ ...ref.current });
   }, [props.text.textInput]);
 
   return (
