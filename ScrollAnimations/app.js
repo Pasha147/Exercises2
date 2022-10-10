f34836863e083fe8f92acaeee4c7fb7b64e6d103
Pasha <75729255+Pasha147@users.noreply.github.com>
2022-10-10 15:48:04 +0300

    ScrollAnim blur can make infinity loop

--- ScrollAnimations/app.js
+++ ScrollAnimations/app.js
@@ -1,7 +1,8 @@
 const observer = new IntersectionObserver(
   (entries) => {
+    console.log(entries);
     entries.forEach((entry) => {
-      console.log(entry);
+      //   console.log(entry);
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
       } else {
