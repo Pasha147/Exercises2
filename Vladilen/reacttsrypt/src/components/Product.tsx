1269910df7f7e2ab34eb22d028909f645661caa8
Pasha147 <pavel147223@gmail.com>
2024-01-20 12:43:57 +0200

    React и TypeScript by Vladilen everything finished except routing

--- Vladilen/reacttsrypt/src/components/Product.tsx
+++ Vladilen/reacttsrypt/src/components/Product.tsx
@@ -1,40 +1,44 @@
 import { useState } from "react"
 import { IProduct } from "../models"
+// import { ModalContext } from "../context/ModalContext"
 
-interface ProductProps{
+interface ProductProps {
     product: IProduct
+
 }
 
 // export function Product(props: ProductProps){
-export function Product({product}: ProductProps){
-    const [details, setDetails]=useState(false)
-    const btnBgClassName= details ? 'bg-yellow-400':'bg-blue-400' 
-    const btnClasses=["py-2 px-4 border", btnBgClassName]
-    return(
+export function Product({ product }: ProductProps) {
+    // const mod = useContext(ModalContext)
+    const [details, setDetails] = useState(false)
+    const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400'
+    const btnClasses = ["py-2 px-4 border", btnBgClassName]
+
+       return (
         <div
-        className="border py-2 px-4 rounded flex 
+            className="border py-2 px-4 rounded flex 
         flex-col item center mb-2"
         >
-            <img src={product.image} className="w-1/6" alt={product.title}/>
-        <p>
-            {product.title}
-        </p>
-        <p className="font-bold">{product.price}</p>
-        <button 
-        onClick={()=>setDetails(prev=>!prev)}
-        className={btnClasses.join(' ')}>
-            {details ? 'Hide Detailes': 'Show Detailes'}
-        </button>
-        {details && <div>
-        <p>{product.description}</p>
-        <p>Rate: <span style={{fontWeight: 'bold'}}>{product?.rating?.rate}</span></p>
-        <button 
-        onClick={()=>setDetails(false)}
-        className="py-2 px-4 border bg-yellow-400">
-            Hide Detailes
-        </button>
-        </div>}
-
+            <img src={product.image} className="w-1/6" alt={product.title} />
+            <p>
+                {product.title}
+            </p>
+            <p className="font-bold">{product.price}</p>
+            <button
+                onClick={() => setDetails(prev => !prev)}
+                className={btnClasses.join(' ')}>
+                {details ? 'Hide Detailes' : 'Show Detailes'}
+            </button>
+            {details && <div>
+                <p>{product.description}</p>
+                <p>Rate: <span style={{ fontWeight: 'bold' }}>{product?.rating?.rate}</span></p>
+                <button
+                    onClick={() => setDetails(false)}
+                    className="py-2 px-4 border bg-yellow-400">
+                    Hide Detailes
+                </button>
+            </div>}
+{/* <pre>{JSON.stringify(mod)}</pre> */}
         </div>
     )
 }
--- Vladilen/reacttsrypt/src/hooks/products.ts
+++ Vladilen/reacttsrypt/src/hooks/products.ts
@@ -3,35 +3,40 @@ import { useEffect, useState } from 'react';
 import axios, { AxiosError } from 'axios';
 import { IProduct } from '.././models';
 
-export function useProducts(){
+export function useProducts() {
 
-    const[products, setProducts]=useState<IProduct[]>([])
-    const [loading, setLoading]=useState(false)
-    const [error, setError]=useState('')
+  const [products, setProducts] = useState<IProduct[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
 
-    function addProduct(product: IProduct){
-      setProducts(prev=>[...prev, product])
+  function addProduct(product: IProduct) {
+    setProducts(prev => [...prev, product])
+  }
+
+  async function fetchProducts() {
+    try {
+      setError('')
+      setLoading(true)
+
+      //Fix CORS Error [SOLVED] 
+      // const conf={headers:{'Access-Control-Allow-Origin': '*'}}
+      // const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5', conf)
+      
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+      setProducts(response.data)
+      setLoading(false)
+    } catch (e: unknown) {
+      const error = e as AxiosError
+      setLoading(false)
+      setError(error.message)
+
     }
-    
-      async function fetchProducts() {
-        try{
-          setError('')
-          setLoading(true)
-          const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-          setProducts(response.data)
-          setLoading(false)
-        }catch(e: unknown){
-          const error =e as AxiosError
-          setLoading(false)
-          setError(error.message)
-    
-        }   
-      }
-    
-      useEffect(() => {
-        fetchProducts()
-      }, [])
-    
-    
-    return {products, error, loading, addProduct}
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+
+  return { products, error, loading, addProduct }
 }
