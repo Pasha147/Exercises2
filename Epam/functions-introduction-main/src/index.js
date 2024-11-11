663a66a811316485230d30088c772c036c043a50
Pasha147 <pavel147223@gmail.com>
2024-11-11 13:52:39 +0200

    Epam completed

--- Epam/functions-introduction-main/src/index.js
+++ Epam/functions-introduction-main/src/index.js
@@ -6,6 +6,12 @@
  */
 module.exports.backToFront = function backToFront(str, symbolsCount) {
     // Your implementation here
+    // let str='asd'; let symbolsCount=3
+    let symb=str.slice(-symbolsCount)
+    
+    str=symbolsCount<=str.length ? symb+str+symb : str
+    // console.log(str);
+    return str
     throw new Error('Task not implemented');
 };
 
@@ -18,6 +24,12 @@ module.exports.backToFront = function backToFront(str, symbolsCount) {
  */
 module.exports.nearest = function nearest(z, x, y) {
     // Your implementation here
+// let z=100; let x=22; let y=122
+let xz=Math.abs(z-x)
+let yz=Math.abs(z-y)
+if(xz<yz) return x;
+if(xz>yz) return y;
+
     throw new Error('Task not implemented');
 };
 
@@ -28,5 +40,16 @@ module.exports.nearest = function nearest(z, x, y) {
  */
 module.exports.removeDuplicate = function removeDuplicate(arr) {
     // Your implementation here
+    let uniqueArr = []; 
+
+    for (let i = 0; i < arr.length; i++) {
+      
+      if (uniqueArr.indexOf(arr[i]) === -1) {
+        uniqueArr.push(arr[i]); 
+      }
+    }
+  
+    return uniqueArr;
+
     throw new Error('Task not implemented');
 };
