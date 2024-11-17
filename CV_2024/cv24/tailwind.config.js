e731c967dbc80d79125616c939fff6918518e40c
Pasha147 <pavel147223@gmail.com>
2024-11-18 00:48:30 +0200

    CV2024 header

--- CV_2024/cv24/tailwind.config.js
+++ CV_2024/cv24/tailwind.config.js
@@ -15,6 +15,18 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+      keyframes:{
+        'open-menu':{
+          '0%': {transform: 'scaleY(0)'},
+          '60%': {transform: 'scaleY(1.4)'},
+          '90%': {transform: 'scaleY(0.9)'},
+          '100%': {transform: 'scaleY(1)'},
+        },
+      },
+      animation:{
+        'open-menu': 'open-menu 0.5s ease-in-out forwards'
+      }
     },
   },
   plugins: [],
